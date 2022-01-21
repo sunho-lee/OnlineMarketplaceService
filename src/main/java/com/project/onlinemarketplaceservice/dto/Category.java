695b286f6a1b83f1239746ae15c58f4fd620856c
sunho-lee <sh112123@naver.com>
2022-01-21 23:59:16 +0900

    기능 : 어플리케이션 준비 이벤트 발생 시 DB에서 카테고리 데이터 가져온다.  매번 데이터베이스에 쿼리를 남기는 것 대신 카테고리 정보를 메모리에 저장하고 사용한다.

---
+++ src/main/java/com/project/onlinemarketplaceservice/dto/Category.java
@@ -0,0 +1,15 @@
+package com.project.onlinemarketplaceservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    Integer id;
+    String name;
+
+}
